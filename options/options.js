707a3b57f6378761c10f3d2da375f4af68b47064
Crowdbotics <team@crowdbotics.com>
2024-02-19 01:31:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "patient-lake-46458",
+  url: "https://patient-lake-46458.botics.co",
+  api: "https://patient-lake-46458.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
